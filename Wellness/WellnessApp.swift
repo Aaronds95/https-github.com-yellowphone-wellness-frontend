a3a8d5c076cbf8dae3812883d85a9c0f1d19d44b
Aaron Schultheis <aaronds95@gmail.com>
2024-02-24 14:28:40 -0500

    Initial Commit

---
+++ Wellness/WellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessApp.swift
+//  Wellness
+//
+//  Created by Aaron Schultheis on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct WellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
